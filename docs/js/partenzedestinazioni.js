d51fca168881949718871115cbe444d76609fd6d
napo <napo@fbk.eu>
2022-07-14 23:19:54 +0200

    update

--- docs/js/partenzedestinazioni.js
+++ docs/js/partenzedestinazioni.js
@@ -7,11 +7,6 @@ $.getJSON('data/partenze_destinazioni.json', function(graph) {
     option_partenzedestinazioni = {
         title: {},
         tooltip: {},
-        legend: [{
-            data: graph.categories.map(function(a) {
-                return a.name;
-            })
-        }],
         animationDurationUpdate: 6000,
         animationEasingUpdate: 'quinticInOut',
         series: [{
@@ -19,31 +14,14 @@ $.getJSON('data/partenze_destinazioni.json', function(graph) {
             orient: 'vertical',
             data: graph.nodes,
             links: graph.links,
-            categories: graph.categories,
             roam: true,
+            emphasis: {
+                focus: 'adjacency'
+            },
             label: {
                 show: true,
                 position: 'right',
                 formatter: '{b}'
-            },
-            lineStyle: {
-                color: 'source',
-                curveness: 0.3
-            },
-            emphasis: {
-                focus: 'adjacency',
-                label: {
-                    position: 'right',
-                    show: true
-                },
-                lineStyle: {
-                    width: 10
-                }
-            },
-            edgeSymbol: ['circle', 'arrow'],
-            edgeSymbolSize: [2, 12],
-            edgeLabel: {
-                fontSize: 10
             }
         }]
     };
