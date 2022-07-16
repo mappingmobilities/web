f06c0b20cf24c47891c6befdb82cf14b8b0067cc
napo <napo@fbk.eu>
2022-07-16 12:19:36 +0200

    update

--- docs/_texts/infoprogetto.md
+++ docs/_texts/infoprogetto.md
@@ -1,9 +1,9 @@
 è un progetto di:
 
 - FBK - [Istituto Storico Italo Germanico](https://isig.fbk.eu/it/)
-    - Anna Grillini
-    - Maurizio Cau
+    - Anna Grillini (research)
+    - Maurizio Cau (project management)
 - FBK - [Digital Commons Lab](https://dcl.fbk.eu)
-    - Maurizo Napolitano
+    - Maurizo Napolitano (data analysis and visualization)
 
 finanziato da [Fondazione CARITRO](https://www.fondazionecaritro.it/component/k2/item/861-bando-post-doc-2019)
--- docs/js/partenzedestinazioni.js
+++ docs/js/partenzedestinazioni.js
@@ -14,9 +14,34 @@ $.getJSON('data/partenze_destinazioni.json', function(graph) {
             orient: 'vertical',
             data: graph.nodes,
             links: graph.links,
-            roam: true,
+            roam: false,
             emphasis: {
                 focus: 'adjacency'
+            },
+            label: {
+                show: true,
+                position: 'bottom',
+                rotate: 90,
+                verticalAlign: 'left',
+                distance: 30,
+                fontFamily: 'DM Sans',
+                color: "#FBEFE5",
+                fontSize: 17
+            },
+            itemStyle: {
+                opacity: 0
+            },
+            lineStyle: {
+                color: "#002254",
+                curveness: 0,
+                opacity: 0.75,
+                width: 1
+            },
+            emphasis: {
+                lineStyle: {
+                    opacity: 0.75,
+                    color: "#FBEFE5"
+                }
             }
         }]
     };
